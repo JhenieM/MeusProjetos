fc9fd6a350cb9c887f4f2af3ea34fa8bba19687d
unknown <lmaday26@gmail.com>
2024-06-04 12:04:32 -0300

    aiaiai

---
+++ Projeto02/API/JS/index.js
@@ -0,0 +1,10 @@
+const api_url = "http:api.openweathermap.org/data/2.5/weather";
+const api_key = "fe5fcd19310a3bf389ede816e310cbeb";
+
+// ?q={city name}&appid={API key}
+
+function submit() {
+    var city = document.getElementById("city").value;
+    console.log(city);
+}
+
