00860b53c4fa0f42d67bf4f6fdb0dfd7d66a1d60
unknown <lmaday26@gmail.com>
2024-06-05 08:03:23 -0300

    em processo slk

--- Projeto02/API/JS/index.js
+++ Projeto02/API/JS/index.js
@@ -1,10 +1,24 @@
 const api_url = "http:api.openweathermap.org/data/2.5/weather";
 const api_key = "fe5fcd19310a3bf389ede816e310cbeb";
 
-// ?q={city name}&appid={API key}
+// http:api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
 function submit() {
     var city = document.getElementById("city").value;
-    console.log(city);
+
+
+const params = new URLSearchParams({
+    q: city,
+    appid: api_key,
+    units: "metric"
+});
+
+console.log (`${api_url}?${params}`);
+
+fetch (`${api_url}?${params}`)
+    .then (function (reponse) {
+        
+   })
+    .then ()
 }
 
