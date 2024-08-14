6e729ada05bf79e380a82d616b9f227732526a67
unknown <lmaday26@gmail.com>
2024-08-14 12:24:48 -0300

    nn vai essa desgraça

--- MP #05 - Tradutor/SCRIPT/script.js
+++ MP #05 - Tradutor/SCRIPT/script.js
@@ -1,3 +1,5 @@
+ //* TexteArea responsivo */
+
 const textarea = document.getElementById('text');
 
         const adjustHeight = () => {
@@ -8,3 +10,36 @@ const textarea = document.getElementById('text');
         textarea.addEventListener('input', adjustHeight);
 
         adjustHeight();
+
+/* Tradutor em tempo real */
+
+//const fetch = require('./node_modules');
+
+const url = "https://libretranslate.com/translate";
+
+const data = {
+    q: "Olá, como você está?",
+    source: "pt",
+    target: "en"
+};
+
+
+fetch(url, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+})
+.then(response => {
+    console.log(response);
+    if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+    }
+    return response.json();
+})
+
+.then(data => console.log(data.translatedText))
+.catch(error => console.error('Error:', error));
+
+
