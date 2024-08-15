eeac2994b7eb97c61c187433343e28ca74582e01
JhenieM <lmaday26@gmail.com>
2024-08-15 10:11:09 -0300

    star

--- MP #05 - Tradutor/JS/script.js
+++ MP #05 - Tradutor/JS/script.js
@@ -2,14 +2,13 @@
 
 const textarea = document.getElementById('text');
 
-        const adjustHeight = () => {
-            textarea.style.height = 'auto';
-            textarea.style.height = `${textarea.scrollHeight}px`;
-        };
+const adjustHeight = () => {
+    textarea.style.height = 'auto';
+    textarea.style.height = `${textarea.scrollHeight}px`;
+};
 
-        textarea.addEventListener('input', adjustHeight);
-
-        adjustHeight();
+textarea.addEventListener('input', adjustHeight);
+adjustHeight();
 
 /* Tradutor em tempo real */
 
@@ -42,4 +41,12 @@ fetch(url, {
 .then(data => console.log(data.translatedText))
 .catch(error => console.error('Error:', error));
 
+/* Passar texto para outro bloco */
+const traduzidoBox = getElementById('tradutorBox');
 
+const passaTexto = () => {
+   traduzidoBox.innnerHTML(textarea); 
+}
+
+traduxorBox = addEventListener('input', passaTexto);
+passaTexto();
