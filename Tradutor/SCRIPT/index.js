4b6190620621e28893f384b334d7d1947f662ff5
JhenieM <lmaday26@gmail.com>
2024-08-29 10:47:43 -0300

    ?@git add .#

---
+++ Tradutor/SCRIPT/index.js
@@ -0,0 +1,25 @@
+/* Função para traduzir o texto */
+
+const selectTag = document.querySelectorAll("select");
+
+selectTag.forEach (tag => {
+        console.log(tag);
+
+        // let selected = id == 0 ? cod_pais == "pt-PT" ? "selected" : "" : cod_pais == "es-ES" ? "selected" : "";
+        // let opcao = `<option ${selected} value="${cod_pais}">${countries[cod_pais]}</options>`;
+        // tag.insertAdjacentHTML("beforeend", opcao); 
+    
+}); 
+
+
+
+/* function submit() {
+    
+}*/
+
+
+// document.addEventListener('keypress', function(event) {
+//   if (event.key === 'Enter') {
+//     submit();
+//   }
+// });
