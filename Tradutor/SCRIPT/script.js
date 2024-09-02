9c4f2598fbd22c996af394f1e8dd455113c17a17
JhenieM <136010804+JhenieM@users.noreply.github.com>
2024-09-02 19:59:14 -0300

    manda pra mãe

--- Tradutor/SCRIPT/script.js
+++ Tradutor/SCRIPT/script.js
@@ -1,20 +1,42 @@
 const selectTag = document.querySelectorAll('select');
+const textoTraduzir = document.querySelector('.text-area');
 
 selectTag.forEach((tag, id) => {
         for (const cod_pais in countries) {
-                let selected = id == 0 ? cod_pais == "pt-PT" ? "selected" : "" : cod_pais == "es-ES" ? "selected" : "";
-                let opcao = `<option ${selected} value="${cod_pais}">${countries[cod_pais]}</options>`;
+                let selected = "";
+
+                if (id == 0) {
+                        if (cod_pais == "pt-PT") {
+                                selected = "selected";
+                        } else {
+                                selected = "";
+                        }
+                } else {
+                        if (cod_pais == "es-ES") {
+                                selected = "selected";
+                        } else {
+                                selected = "";
+                        }
+                }
+
+                let opcao = `<option ${selected} value="${cod_pais}">${countries[cod_pais]}</option>`;
                 tag.insertAdjacentHTML("beforeend", opcao);
         }
 });
 
 function submit() {
-        
+        let text = textoTraduzir.value;
+        let traduzirDe = selectTag[0].value;
+        let traduzirPara = selectTag[1].value;
+        const apiURL = `https://api.mymemory.translated.net/get?q=${text}&langpair=${traduzirDe}|${traduzirPara};`
+
+        fetch(apiURL)
+        .then(resposnse => {
+                return resposnse.json();
+        })
+        .then(data => {
+                console.log(data);
+        })
 }
 
-
-// document.addEventListener('keypress', function(event) {
-//   if (event.key === 'Enter') {
-//     submit();
-//   }
-// });
+// https://api.mymemory.translated.net/get?q=Hello%20World!&langpair=en|it
