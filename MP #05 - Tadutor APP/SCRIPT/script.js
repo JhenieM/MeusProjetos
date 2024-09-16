0ad65e3c2e32df5966ebee0f570e13d7c7f0dc4b
JhenieM <lmaday26@gmail.com>
2024-09-16 10:47:08 -0300

    cadeira

--- MP #05 - Tadutor APP/SCRIPT/script.js
+++ MP #05 - Tadutor APP/SCRIPT/script.js
@@ -1,15 +1,24 @@
-/* TexteArea responsivo 
+const speak = document.getElementsByClassName('btn-audio');
 
-var textArea = document.querySelectorAll('textarea');
+/*TexteArea responsivo*/ 
+const textarea = document.getElementById('textMain');
+const textareas = document.getElementById('textOther');
 
-const tamanhoTraduzir = () => {
-    textArea.style.height = 'auto';
-    textArea.style.height = `${textArea.scrollHeight}px`;
-};
+        const adjustHeight = () => {
+            textarea.style.height = 'auto';
+            textarea.style.height = `${textarea.scrollHeight}px`;
+            textarea.style.margin = '10px';
 
-textArea.addEventListener('input', tamanhoTraduzir);
-tamanhoTraduzir();*/
+            textareas.style.height = 'auto';
+            textareas.style.height = `${textarea.scrollHeight}px`;
+            textareas.style.margin = '10px';
+        };
 
+        textarea.addEventListener('input', adjustHeight);
+        textareas.addEventListener('input', adjustHeight);
+        adjustHeight();
+
+/*TRADUTOR*/
 const selectTag = document.querySelectorAll('select');
 const textoTraduzir = document.querySelector('.textFrom');
 const traduzido = document.querySelector('.textTo');
@@ -17,7 +26,6 @@ const traduzido = document.querySelector('.textTo');
 selectTag.forEach((tag, id) => {
         for (const cod_pais in countries) {
                 let selected = "";
-
                 if (id == 0) {
                         if (cod_pais == "pt-BR") {
                                 selected = "selected";
@@ -40,6 +48,7 @@ selectTag.forEach((tag, id) => {
 
 function submit() {
         if (textoTraduzir.value != "") {
+                speak.style.display = 'block';
                 let text = textoTraduzir.value;
                 let traduzirDe = selectTag[0].value;
                 let traduzirPara = selectTag[1].value;
@@ -55,9 +64,10 @@ function submit() {
                         //data.responseData.translatedText
                         traduzido.innerHTML = data.responseData.translatedText;
                 });
-        }     
+        }
 }
 
+/*Função de Audio*/
 function audio() {
         if(traduzido.value != '') {
                 let aud = new SpeechSynthesisUtterance(traduzido.value);
@@ -70,7 +80,7 @@ function contagem() {
         document.querySelector('.contagem').innerText = `${caracteres.length}/600`;
 
 
-        if(caracteres.length == 5){
+        if(caracteres.length == 600){
                 textoTraduzir.disabled = true;
                 caracteres.style.color = 'red'; 
                 //document.getElementById("obs").disabled = enable;
