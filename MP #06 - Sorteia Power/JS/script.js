d536fd8b48a469391ac62f151f2a4533395fcfcf
unknown <lmaday26@gmail.com>
2024-09-24 12:26:35 -0300

    majessica

--- MP #06 - Sorteia Power/JS/script.js
+++ MP #06 - Sorteia Power/JS/script.js
@@ -2,9 +2,16 @@
 letras = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
 const boxLetras = document.getElementById('sorteadas');
+const colorBall = getComputedStyle(span).getPropertyValue('--color');
+
+var green = 'rgb(1 137 39)';
+var blue = 'rgb(0 77 123)';
+var yellow = 'rgb(244 241 100)';
+var red = 'rgb(247 0 87)';
 
 function sortear() {
     boxLetras.style.display = "flex";
+
     function choose(letras) {
         var index = Math.floor(Math.random() * letras.length);
         var letra = letras[index];
@@ -18,8 +25,21 @@ function sortear() {
         const sorteada = document.getElementById('sorteadas');
         sorteada.appendChild(span);
     }
+    function colors() {
+        if(colorBall === blue) {
+            var hai = document.body.style.setProperty('--color', green);
+        }else if (colorBall === green) {
+            var hai = document.body.style.setProperty('--color', red);
+        }else if (colorBall === red) {
+            var hai = document.body.style.setProperty('--color', yellow);
+        }else {
+            var hai =  document.body.style.setProperty('--color', blue);
+        }
+        console.log(hai)
+    }
     choose(letras);
     save();
+    colors();
 }
 
 //console.log(choose(letras));
