4cae35553d3871e0ff777d32fa12aa8aa6e4a088
JhenieM <136010804+JhenieM@users.noreply.github.com>
2024-09-25 22:28:26 -0300

    fases finais

--- MP #06 - Sorteia Power/JS/script.js
+++ MP #06 - Sorteia Power/JS/script.js
@@ -1,34 +1,32 @@
-/*Sortear */
-letras = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+letras = ['A','B','C','D','E','F','G','I','L','O','P','R','S','T','X','Y'];
 
 const boxLetras = document.getElementById('sorteadas');
-const colorBall = getComputedStyle(span).getPropertyValue('--color');
-
 var cores = ['rgb(1 137 39)', 'rgb(0 77 123)', 'rgb(190 186 0)', 'rgb(247 0 87)'];
 var indice = 0;
 
 function sortear() {
     boxLetras.style.display = "flex";
 
+    //Sortear letras
     function choose(letras) {
         var index = Math.floor(Math.random() * letras.length);
         var letra = letras[index];
         return span = document.getElementById('span').innerText = `${letra}`;
     }
+
+    //Salvar e exibir na tela a letra sortida
     function save() {
         const span = document.createElement('span');
-        span.innerText = `${choose(letras)}`;
-        span.id = 'caixa-sorteadas';
+        span.innerText = choose(letras);
+        span.classList.add('caixa-sorteadas');
 
-        const sorteada = document.getElementById('sorteadas');
-        sorteada.appendChild(span);
+        boxLetras.appendChild(span);
 
         span.style.backgroundColor = cores[indice];
         indice = (indice + 1) % cores.length;
     }
 
+    //Chamando as funções quando o botão for clicado
     choose(letras);
     save();
 }
-
-//console.log(choose(letras));
